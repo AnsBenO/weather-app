d158da78d0d242edec0e4a0cfc496b96ecd7f630
AnsBenO <138506861+AnsBenO@users.noreply.github.com>
2023-07-31 12:51:29 +0100

    Update Forecast.tsx

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -41,23 +41,23 @@ const Forecast: React.FC<ForecastProps> = ({ data }) => {
                             <div className="hourly-detail-grid">
                                 <div className="hourly-detail-grid-item">
                                     <label >Pressure</label>
-                                    <label >{item.main.pressure}hPa</label>
+                                    <label >{item.main.pressure} hPa</label>
                                 </div>
                                 <div className="hourly-detail-grid-item">
                                     <label >Humidity</label>
-                                    <label >{item.main.humidity}%</label>
+                                    <label >{item.main.humidity} %</label>
                                 </div>
                                 <div className="hourly-detail-grid-item">
                                     <label >Clouds</label>
-                                    <label >{item.clouds.all}%</label>
+                                    <label >{item.clouds.all} %</label>
                                 </div>
                                 <div className="hourly-detail-grid-item">
                                     <label >Wind speed</label>
-                                    <label >{item.wind.speed}m/s</label>
+                                    <label >{item.wind.speed} m/s</label>
                                 </div>
                                 <div className="hourly-detail-grid-item">
                                     <label >Sea level</label>
-                                    <label >{item.main.sea_level}m</label>
+                                    <label >{item.main.sea_level} m</label>
                                 </div>
                                 <div className="hourly-detail-grid-item">
                                     <label >Feels like</label>
