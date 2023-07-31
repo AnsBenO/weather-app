63095c908a8d102b03279a1583f80b5819e4a374
AnsBenO <138506861+AnsBenO@users.noreply.github.com>
2023-07-31 11:53:25 +0100

    Update Forecast.tsx

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -33,23 +33,23 @@ const Forecast: React.FC<ForecastProps> = ({ data }) => {
                             <div className="daily-detail-grid">
                                 <div className="daily-detail-grid-item">
                                     <label >Pressure</label>
-                                    <label >{item.main.pressure}hPa</label>
+                                    <label >{item.main.pressure} hPa</label>
                                 </div>
                                 <div className="daily-detail-grid-item">
                                     <label >Humidity</label>
-                                    <label >{item.main.humidity}%</label>
+                                    <label >{item.main.humidity} %</label>
                                 </div>
                                 <div className="daily-detail-grid-item">
                                     <label >Clouds</label>
-                                    <label >{item.clouds.all}%</label>
+                                    <label >{item.clouds.all} %</label>
                                 </div>
                                 <div className="daily-detail-grid-item">
                                     <label >Wind speed</label>
-                                    <label >{item.wind.speed}m/s</label>
+                                    <label >{item.wind.speed} m/s</label>
                                 </div>
                                 <div className="daily-detail-grid-item">
                                     <label >Sea level</label>
-                                    <label >{item.main.sea_level}m</label>
+                                    <label >{item.main.sea_level} m</label>
                                 </div>
                                 <div className="daily-detail-grid-item">
                                     <label >Feels like</label>
